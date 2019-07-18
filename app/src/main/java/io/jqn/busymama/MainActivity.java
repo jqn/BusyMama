d155fda492dc9fb36d359f26aa8476dda6a339c4
jQN <jqn@jQNs-MacBook-Pro.local>
2019-07-18 08:17:55 +0200

    chore: configure firebase core and cloud firestore Resolves: #1

--- app/src/main/java/io/jqn/busymama/MainActivity.java
+++ app/src/main/java/io/jqn/busymama/MainActivity.java
@@ -1,6 +1,7 @@
 package io.jqn.busymama;
 
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {

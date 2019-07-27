829f1caec7fdeee2a3e72910ab9f787bc9f61f1e
jQN <jqn@jQNs-MacBook-Pro.local>
2019-07-27 13:17:45 +0200

    feat: add drawer to dashboard activity
    issue: #6

--- app/src/main/java/io/jqn/busymama/MainActivity.java
+++ app/src/main/java/io/jqn/busymama/MainActivity.java
@@ -37,7 +37,7 @@ import java.util.List;
 import io.jqn.busymama.provider.PlaceContract;
 import io.jqn.busymama.utilities.NotificationUtils;
 
-public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+public class MainActivity extends AppCompatActivity  {
 
     // Constants
     public static final String TAG = MainActivity.class.getSimpleName();
@@ -53,157 +53,10 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Places.initialize(getApplicationContext(), "AIzaSyA9uKizzenbbzhrtWYwqbIzmC0ggH-BurA" );
-        PlacesClient placesClient = Places.createClient(this   );
-
-        // Build up the LocationServices API client
-        // Uses the addApi method to request the LocationServices API
-        // Also uses enableAutoManage to automatically know when to connect/suspend the client
-//        GoogleApiClient client = new GoogleApiClient.Builder(this)
-//                .addConnectionCallbacks(this)
-//                .addOnConnectionFailedListener(this)
-//                .addApi(LocationServices.API)
-//                .addApi(Places.GEO_DATA_API)
-//                .enableAutoManage(this, this)
-//                .build();
-    }
-
-    /***
-     * Called when the Google API Client is successfully connected
-     *
-     * @param connectionHint Bundle of data provided to clients by Google Play services
-     */
-    @Override
-    public void onConnected(@Nullable Bundle connectionHint) {
-        refreshPlacesData();
-        Log.i(TAG, "API Client Connection Successful!");
-    }
-
-    /***
-     * Called when the Google API Client is suspended
-     *
-     * @param cause cause The reason for the disconnection. Defined by constants CAUSE_*.
-     */
-    @Override
-    public void onConnectionSuspended(int cause) {
-        Log.i(TAG, "API Client Connection Suspended!");
-    }
-
-    /***
-     * Called when the Google API Client failed to connect to Google Play Services
-     *
-     * @param result A ConnectionResult that can be used for resolving the error
-     */
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult result) {
-        Log.e(TAG, "API Client Connection Failed!");
-    }
-
-
-    public void refreshPlacesData() {
-        Uri uri = PlaceContract.PlaceEntry.CONTENT_URI;
-        Cursor data = getContentResolver().query(
-                uri,
-                null,
-                null,
-                null,
-                null);
-
-        if (data == null || data.getCount() == 0) return;
-        List<String> guids = new ArrayList<String>();
-        while (data.moveToNext()) {
-            guids.add(data.getString(data.getColumnIndex(PlaceContract.PlaceEntry.COLUMN_PLACE_ID)));
-        }
-//        PendingResult<PlaceBuffer> placeResult = Places.GeoDataApi.getPlaceById(mClient,
-//                guids.toArray(new String[guids.size()]));
-//        placeResult.setResultCallback(new ResultCallback<PlaceBuffer>() {
-//            @Override
-//            public void onResult(@NonNull PlaceBuffer places) {
-//                mAdapter.swapPlaces(places);
-//
-//            }
-//        });
-    }
-
-
-    /***
-     * Button Click event handler to handle clicking the "Add new location" Button
-     *
-     * @param view
-     */
-    public void onAddPlaceButtonClicked(View view) {
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            Toast.makeText(this, getString(R.string.need_location_permission_message), Toast.LENGTH_LONG).show();
-            return;
-        }
-//        try {
-//            // Start a new Activity for the Place Picker API, this will trigger {@code #onActivityResult}
-//            // when a place is selected or with the user cancels.
-//            PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
-//            Intent i = builder.build(this);
-//            startActivityForResult(i, PLACE_PICKER_REQUEST);
-//        } catch (GooglePlayServicesNotAvailableException e) {
-//            Log.e(TAG, String.format("GooglePlayServices Not Available [%s]", e.getMessage()));
-//        } catch (Exception e) {
-//            Log.e(TAG, String.format("PlacePicker Exception: %s", e.getMessage()));
-//        }
 
     }
 
 
-    /***
-     * Called when the Place Picker Activity returns back with a selected place (or after canceling)
-     *
-     * @param requestCode The request code passed when calling startActivityForResult
-     * @param resultCode  The result code specified by the second activity
-     * @param data        The Intent that carries the result data.
-     */
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        if (requestCode == PLACE_PICKER_REQUEST && resultCode == RESULT_OK) {
-//            Place place = PlacePicker.getPlace(this, data);
-//            if (place == null) {
-//                Log.i(TAG, "No place selected");
-//                return;
-//            }
-//
-//            // Extract the place information from the API
-//            String placeName = place.getName().toString();
-//            String placeAddress = place.getAddress().toString();
-//            String placeID = place.getId();
-//
-//            // Insert a new place into DB
-//            ContentValues contentValues = new ContentValues();
-//            contentValues.put(PlaceContract.PlaceEntry.COLUMN_PLACE_ID, placeID);
-//            getContentResolver().insert(PlaceContract.PlaceEntry.CONTENT_URI, contentValues);
-//        }
-    }
-
-
-    @Override
-    public void onResume() {
-        super.onResume();
-
-        // Initialize location permissions checkbox
-        CheckBox locationPermissions = (CheckBox) findViewById(R.id.location_permission_checkbox);
-        if (ActivityCompat.checkSelfPermission(MainActivity.this,
-                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            locationPermissions.setChecked(false);
-        } else {
-            locationPermissions.setChecked(true);
-            locationPermissions.setEnabled(false);
-        }
-    }
-
-    public void onLocationPermissionClicked(View view) {
-        ActivityCompat.requestPermissions(MainActivity.this,
-                new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
-                PERMISSIONS_REQUEST_FINE_LOCATION);
-    }
-
-    public void testNotification(View view) {
-        NotificationUtils.remindUserNearShop(this);
-    }
 
 }
 

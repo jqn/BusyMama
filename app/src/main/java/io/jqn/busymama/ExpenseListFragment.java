08c05d41e8bbbf94137e6ec3b8529447b18f7867
jQN <jqn@jQNs-MacBook-Pro.local>
2019-08-02 07:41:58 +0200

    feat: implement recycler view for expense list
    issue: #6

--- app/src/main/java/io/jqn/busymama/ExpenseListFragment.java
+++ app/src/main/java/io/jqn/busymama/ExpenseListFragment.java
@@ -1,36 +1,101 @@
 package io.jqn.busymama;
 
 import android.content.Context;
-import android.net.Uri;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 /**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ExpenseListFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ExpenseListFragment#newInstance} factory method to
- * create an instance of this fragment.
+ * Provides UI for the expenses view.
  */
 public class ExpenseListFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        RecyclerView recyclerView = (RecyclerView) inflater.inflate(
+                R.layout.recycler_view, container, false);
+        ContentAdapter adapter = new ContentAdapter(recyclerView.getContext());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        return recyclerView;
 
+    }
 
-    public ExpenseListFragment() {
-        // Required empty public constructor
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView avatar;
+        public TextView name;
+        public TextView description;
+
+        public ViewHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.fragment_expense_list, parent, false));
+            avatar =  itemView.findViewById(R.id.list_avatar);
+            name = itemView.findViewById(R.id.list_title);
+            description = itemView.findViewById(R.id.list_desc);
+            itemView.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    Context context = v.getContext();
+                    Intent intent = new Intent(context, ExpenseDetailActivity.class);
+                    intent.putExtra(ExpenseDetailActivity.EXTRA_POSITION, getAdapterPosition());
+                    context.startActivity(intent);
+                }
+            });
+        }
+    }
+
+    /**
+     * Adapter to display recycler view.
+     */
+    public static class ContentAdapter extends RecyclerView.Adapter<ViewHolder> {
+        // Ste numbers of List in RecyclerView
+        private static final int LENGTH = 18;
+
+        private final String[] mPlaces;
+        private final String[] mPlaceDesc;
+        private final Drawable[] mPlaceAvatars;
+
+        public ContentAdapter(Context context) {
+            Resources resources = context.getResources();
+            mPlaces = resources.getStringArray(R.array.places);
+            mPlaceDesc = resources.getStringArray(R.array.place_desc);
+            TypedArray a = resources.obtainTypedArray(R.array.place_avator);
+            mPlaceAvatars = new Drawable[a.length()];
+            for (int i = 0; i < mPlaceAvatars.length; i++) {
+                mPlaceAvatars[i] = a.getDrawable(i);
+            }
+            a.recycle();
+        }
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new ViewHolder(LayoutInflater.from(parent.getContext()), parent);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            holder.avatar.setImageDrawable(mPlaceAvatars[position % mPlaceAvatars.length]);
+            holder.name.setText(mPlaces[position % mPlaces.length]);
+            holder.description.setText(mPlaceDesc[position % mPlaceDesc.length]);
+        }
+
+        @Override
+        public int getItemCount() {
+            return LENGTH;
+        }
     }
 
 }

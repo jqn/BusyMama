7c2d91e0e147090c563c3598a8898e9bb905ccb7
jQN <jqn@jQNs-MacBook-Pro.local>
2019-08-06 22:34:04 +0200

    refactor: move transactionAdapter into it's own file
    issue: #7

--- app/src/main/java/io/jqn/busymama/adapters/TransactionAdapter.java
+++ app/src/main/java/io/jqn/busymama/adapters/TransactionAdapter.java
@@ -1,4 +1,140 @@
 package io.jqn.busymama.adapters;
 
-public class TransactionAdapter {
+import android.content.Context;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Locale;
+
+import io.jqn.busymama.R;
+import io.jqn.busymama.database.TransactionEntry;
+import io.jqn.busymama.fragments.TransactionListFragment;
+import timber.log.Timber;
+
+/**
+ * This TransactionAdapter creates and binds ViewHolders, that hold the amount and description of a transaction,
+ * to a RecyclerView to efficiently display data.
+ */
+public class TransactionAdapter extends RecyclerView.Adapter<TransactionAdapter.TransactionViewHolder> {
+
+    // Constant for date format
+    private static final String DATE_FORMAT = "dd/MM/yyy";
+
+    // Member variable to handle item clicks
+    final private ItemClickListener mItemClickListener;
+    // Class variables for the List that holds transaction data and the Context
+    private List<TransactionEntry> mTransactionEntries;
+    private Context mContext;
+    // Date formatter
+    private SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT, Locale.getDefault());
+
+    /**
+     * Constructor for the TransactionAdapter that initializes the Context.
+     *  @param context  the current Context
+     * @param listener the ItemClickListener
+     */
+    public TransactionAdapter(Context context, ItemClickListener listener) {
+        mContext = context;
+        mItemClickListener = listener;
+    }
+
+    /**
+     * Called when ViewHolders are created to fill a RecyclerView.
+     *
+     * @return A new TransactionViewHolder that holds the view for each transaction
+     */
+    @Override
+    public TransactionViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // Inflate the transaction_layout to a view
+        View view = LayoutInflater.from(mContext)
+                .inflate(R.layout.transaction_list, parent, false);
+
+        return new TransactionViewHolder(view);
+    }
+
+    /**
+     * Called by the RecyclerView to display data at a specified position in the Cursor.
+     *
+     * @param holder   The ViewHolder to bind Cursor data to
+     * @param position The position of the data in the Cursor
+     */
+    @Override
+    public void onBindViewHolder(TransactionViewHolder holder, int position) {
+        // Determine the values of the wanted data
+        TransactionEntry transactionEntry = mTransactionEntries.get(position);
+        String description = transactionEntry.getAmount();
+        int priority = transactionEntry.getPriority();
+        String updatedAt = dateFormat.format(transactionEntry.getUpdatedAt());
+
+        //Set values
+        holder.transactionDescriptionView.setText(description);
+        holder.updatedAtView.setText(updatedAt);
+
+        // Programmatically set the text and color for the priority TextView
+        String priorityString = "" + priority; // converts int to String
+        holder.priorityView.setText(priorityString);
+
+    }
+
+
+    /**
+     * Returns the number of items to display.
+     */
+    @Override
+    public int getItemCount() {
+        if (mTransactionEntries == null) {
+            return 0;
+        }
+        Timber.d("entries" + mTransactionEntries.size() );
+        return mTransactionEntries.size();
+    }
+
+    /**
+     * When data changes, this method updates the list of transactionEntries
+     * and notifies the adapter to use the new values on it
+     */
+    public void setTransactions(List<TransactionEntry> transactionEntries) {
+        mTransactionEntries = transactionEntries;
+        notifyDataSetChanged();
+    }
+
+    public interface ItemClickListener {
+        void onItemClickListener(int itemId);
+    }
+
+    // Inner class for creating ViewHolders
+    class TransactionViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        // Class variables for the transaction description and priority TextViews
+        TextView transactionDescriptionView;
+        TextView updatedAtView;
+        TextView priorityView;
+
+        /**
+         * Constructor for the TransactionViewHolder.
+         *
+         * @param itemView The view inflated in onCreateViewHolder
+         */
+        public TransactionViewHolder(View itemView) {
+            super(itemView);
+
+            transactionDescriptionView = itemView.findViewById(R.id.list_title);
+            updatedAtView = itemView.findViewById(R.id.list_desc);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            int elementId = mTransactionEntries.get(getAdapterPosition()).getId();
+            mItemClickListener.onItemClickListener(elementId);
+        }
+    }
 }
--- app/src/main/java/io/jqn/busymama/fragments/TransactionListFragment.java
+++ app/src/main/java/io/jqn/busymama/fragments/TransactionListFragment.java
@@ -1,10 +1,6 @@
 package io.jqn.busymama.fragments;
 
 import android.content.Context;
-import android.content.Intent;
-import android.content.res.Resources;
-import android.content.res.TypedArray;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -14,24 +10,35 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import io.jqn.busymama.R;
-import io.jqn.busymama.TransactionDetailActivity;
+import io.jqn.busymama.adapters.TransactionAdapter;
 
 /**
  * Provides UI for the expenses view.
  */
-public class TransactionListFragment extends Fragment {
+public class TransactionListFragment extends Fragment implements TransactionAdapter.ItemClickListener {
+    private Context mContext;
+
+    public TransactionListFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mContext=context;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Get a reference to the RecyclerView from xml
         RecyclerView recyclerView = (RecyclerView) inflater.inflate(
                 R.layout.recycler_view, container, false);
-        ContentAdapter adapter = new ContentAdapter(recyclerView.getContext());
+        // Initialize the adapter and attach it to the RecyclerView
+        TransactionAdapter adapter = new TransactionAdapter(recyclerView.getContext(), this);
         recyclerView.setAdapter(adapter);
+
         recyclerView.setHasFixedSize(true);
         // Set the LayoutManager for measuring and positioning items views withing the
         // RecyclerView into a linear list
@@ -40,67 +47,73 @@ public class TransactionListFragment extends Fragment {
 
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        public ImageView avatar;
-        public TextView name;
-        public TextView description;
-
-        public ViewHolder(LayoutInflater inflater, ViewGroup parent) {
-            super(inflater.inflate(R.layout.expense_list, parent, false));
-            avatar =  itemView.findViewById(R.id.list_avatar);
-            name = itemView.findViewById(R.id.list_title);
-            description = itemView.findViewById(R.id.list_desc);
-            itemView.setOnClickListener(new View.OnClickListener(){
-                @Override
-                public void onClick(View v) {
-                    Context context = v.getContext();
-                    Intent intent = new Intent(context, TransactionDetailActivity.class);
-                    intent.putExtra(TransactionDetailActivity.EXTRA_POSITION, getAdapterPosition());
-                    context.startActivity(intent);
-                }
-            });
-        }
+    @Override
+    public void onItemClickListener(int itemId) {
+        // Launch AddTaskActivity adding the itemId as an extra in the intent
     }
 
-    /**
-     * Adapter to display recycler view.
-     */
-    public static class ContentAdapter extends RecyclerView.Adapter<ViewHolder> {
-        // Ste numbers of List in RecyclerView
-        // TODO make this dynamic
-        private static final int LENGTH = 6;
+//
+//    public static class ViewHolder extends RecyclerView.ViewHolder {
+//        public ImageView avatar;
+//        public TextView name;
+//        public TextView description;
+//
+//        public ViewHolder(LayoutInflater inflater, ViewGroup parent) {
+//            super(inflater.inflate(R.layout.transaction_list, parent, false));
+//            avatar =  itemView.findViewById(R.id.list_avatar);
+//            name = itemView.findViewById(R.id.list_title);
+//            description = itemView.findViewById(R.id.list_desc);
+//            itemView.setOnClickListener(new View.OnClickListener(){
+//                @Override
+//                public void onClick(View v) {
+//                    Context context = v.getContext();
+//                    Intent intent = new Intent(context, TransactionDetailActivity.class);
+//                    intent.putExtra(TransactionDetailActivity.EXTRA_POSITION, getAdapterPosition());
+//                    context.startActivity(intent);
+//                }
+//            });
+//        }
+//    }
 
-        private final String[] mPlaces;
-        private final String[] mPlaceDesc;
-        private final Drawable[] mPlaceAvatars;
-
-        public ContentAdapter(Context context) {
-            Resources resources = context.getResources();
-            mPlaces = resources.getStringArray(R.array.expenses);
-            mPlaceDesc = resources.getStringArray(R.array.store_details);
-            TypedArray a = resources.obtainTypedArray(R.array.place_avator);
-            mPlaceAvatars = new Drawable[a.length()];
-            for (int i = 0; i < mPlaceAvatars.length; i++) {
-                mPlaceAvatars[i] = a.getDrawable(i);
-            }
-            a.recycle();
-        }
-        @Override
-        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            return new ViewHolder(LayoutInflater.from(parent.getContext()), parent);
-        }
-
-        @Override
-        public void onBindViewHolder(ViewHolder holder, int position) {
-            holder.avatar.setImageDrawable(mPlaceAvatars[position % mPlaceAvatars.length]);
-            holder.name.setText(mPlaces[position % mPlaces.length]);
-            holder.description.setText(mPlaceDesc[position % mPlaceDesc.length]);
-        }
-
-        @Override
-        public int getItemCount() {
-            return LENGTH;
-        }
-    }
+//    /**
+//     * Adapter to display recycler view.
+//     */
+//    public static class ContentAdapter extends RecyclerView.Adapter<ViewHolder> {
+//        // Ste numbers of List in RecyclerView
+//        // TODO make this dynamic
+//        private static final int LENGTH = 6;
+//
+//        private final String[] mPlaces;
+//        private final String[] mPlaceDesc;
+//        private final Drawable[] mPlaceAvatars;
+//
+//        public ContentAdapter(Context context) {
+//            Resources resources = context.getResources();
+//            mPlaces = resources.getStringArray(R.array.expenses);
+//            mPlaceDesc = resources.getStringArray(R.array.store_details);
+//            TypedArray a = resources.obtainTypedArray(R.array.place_avator);
+//            mPlaceAvatars = new Drawable[a.length()];
+//            for (int i = 0; i < mPlaceAvatars.length; i++) {
+//                mPlaceAvatars[i] = a.getDrawable(i);
+//            }
+//            a.recycle();
+//        }
+//        @Override
+//        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+//            return new ViewHolder(LayoutInflater.from(parent.getContext()), parent);
+//        }
+//
+//        @Override
+//        public void onBindViewHolder(ViewHolder holder, int position) {
+//            holder.avatar.setImageDrawable(mPlaceAvatars[position % mPlaceAvatars.length]);
+//            holder.name.setText(mPlaces[position % mPlaces.length]);
+//            holder.description.setText(mPlaceDesc[position % mPlaceDesc.length]);
+//        }
+//
+//        @Override
+//        public int getItemCount() {
+//            return LENGTH;
+//        }
+//    }
 
 }

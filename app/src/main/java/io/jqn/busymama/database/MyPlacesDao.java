9b82424dbf45e095e11f9e35dc50d413dfca10ff
jQN <jqn@jQNs-MacBook-Pro.local>
2019-08-22 18:23:35 +0200

    feature: create a myplaces dao
    issue: #8

---
+++ app/src/main/java/io/jqn/busymama/database/MyPlacesDao.java
@@ -0,0 +1,29 @@
+package io.jqn.busymama.database;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface MyPlacesDao {
+    @Query("SELECT * FROM `myplaces` ORDER BY id")
+    LiveData<List<MyPlacesEntry>> loadAllPlaces();
+
+    @Insert
+    void insertMyPlace(MyPlacesEntry myPlacesEntry);
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    void updateMyPlace(MyPlacesEntry myPlacesEntry);
+
+    @Delete
+    void deleteMyPlace(MyPlacesEntry myPlacesEntry);
+
+    @Query("SELECT * FROM `myplaces` WHERE id = :id")
+    LiveData<MyPlacesEntry> loadMyPlacesById(int id);
+}
